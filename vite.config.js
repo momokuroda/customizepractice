b899222fe15c10a47e233b71a1bfcfd2ec4061eb
momokuroda <129722941+momokuroda@users.noreply.github.com>
2024-08-21 16:28:04 +0900

    微修正

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default {
                 customizepractice: 'src/customizepractice.js',
                 問診表: 'src/問診表.js',
             },
-            // 「vite-kintone.js」 を起点にビルドする
             output: {
                 // format: 'iife', // 即時実行関数
                 dir: 'dist', // 「dist」ディレクトリの下にビルド後のファイルを生成する
